a1826f20c890f08c1fdd70516c0a685a3ad236e2
Swislok-Dev <cmarker4@gmail.com>
2022-05-31 11:36:09 -0500

    init deploy-commands

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nServer created on: ${interaction.guild.createdAt}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
+})
+
 // respond to message
 client.on('messageCreate', (message) => {
   listener.onMessage(message)
 })
 
 // Login to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.TOKEN)
